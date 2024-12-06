cd57257a80657c2682b1c4d3da79921c7f4d04b5
myeongsoyi <myeongsoyi@gmail.com>
2024-12-06 10:35:52 +0900

    feat: app.py, requiremets.txt 수정

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ import logging
 # 블루프린트 임포트
 from route.user_route import user_page
 from route.dream_page import bp as dream_page
-from music.music import music_bp
+# from music.music import music_bp
 
 # 로깅 설정
 logging.basicConfig(level=logging.INFO)
@@ -45,7 +45,7 @@ app.secret_key = 'zl934h23i23I23lsc94b'
 # 블루프린트 등록
 app.register_blueprint(user_page)
 app.register_blueprint(dream_page)
-app.register_blueprint(music_bp)
+# app.register_blueprint(music_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
